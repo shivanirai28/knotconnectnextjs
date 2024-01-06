1ae4f6eaae27862e617336b3bb45f194f85919a6
DELL <shivanikumari28@gmail.com>
2024-01-07 02:20:49 +0530

    stuck on topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,8 @@
+import { OrganizationSwitcher, SignedIn, SignOutButton  } from "@clerk/nextjs";
 import Link from "next/link";
 
 function Topbar() {
+  const isUserLoggedIn = true;
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
@@ -9,6 +11,32 @@ function Topbar() {
           KnotConnect
         </p>
       </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <img
+                  src="/assets/logout.svg" 
+                  alt="logout"
+                  width={24}
+                  height={24}
+                  />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+        appearance={{
+          elements: {
+            organizationSwitcherTrigger:
+            "py-2 px-4"
+          }
+        }}
+        />
+      </div>
     </nav>
   );
 }
