82dea21b6e9d4701a2c0bf448413e3bdf649cd2b
DELL <shivanikumari28@gmail.com>
2024-01-07 16:50:14 +0530

    fixed clerk issue

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,12 +1,15 @@
-import { OrganizationSwitcher, SignedIn, SignOutButton  } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Image from "next/image";
 import Link from "next/link";
 
 function Topbar() {
   const isUserLoggedIn = true;
+
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <img src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">
           KnotConnect
         </p>
@@ -17,24 +20,25 @@ function Topbar() {
           <SignedIn>
             <SignOutButton>
               <div className="flex cursor-pointer">
-                <img
-                  src="/assets/logout.svg" 
+                <Image
+                  src="/assets/logout.svg"
                   alt="logout"
                   width={24}
                   height={24}
-                  />
+                />
               </div>
             </SignOutButton>
           </SignedIn>
+          
         </div>
 
         <OrganizationSwitcher
-        appearance={{
-          elements: {
-            organizationSwitcherTrigger:
-            "py-2 px-4"
-          }
-        }}
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
         />
       </div>
     </nav>
