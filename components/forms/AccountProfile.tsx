1f569642318159b2da88df00349629c6c4b8e305
DELL <shivanikumari28@gmail.com>
2024-01-12 21:51:14 +0530

    initial onboarding setup done

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import {
+    Form
+  } from "@/components/ui/form";
+
+import { useForm } from "react-hook-form";
+
+interface Props {
+    user: {
+      id: string;
+      objectId: string;
+      username: string;
+      name: string;
+      bio: string;
+      image: string;
+    };
+    btnTitle: string;
+  }
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
+
+    const form = useForm()
+    return (
+        <Form>
+
+        </Form>
+    )
+}
+
+export default AccountProfile;
