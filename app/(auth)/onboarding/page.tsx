f6571d7c7d9e6a9b3a21b64f9d4737a2c429290a
DELL <shivanikumari28@gmail.com>
2024-01-14 21:08:31 +0530

    onboarding initial setup done

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -2,10 +2,9 @@ import AccoutProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
 
 async function Page() {
-  const user  ();
+  const user = await currentUser();
 
-  const userInfo =();
-
+  const userInfo = {};
 
   const userData = {
     id: user?.id,
@@ -16,19 +15,18 @@ async function Page() {
     image: userInfo?.image || user?.imageUrl,
   };
 
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Welcome User...</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now, to use KnotConnect.
+      </p>
 
-    return  (
-      <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
-        <h1 className="head-text">Welcome good sir...</h1>
-        <p className="mt-3 text-base-regular text-light-2">
-          Complete your profile now, to use KnotConnect.
-        </p>
-  
-        <section className="mt-9 bg-dark-2 p-10">
-          <AccoutProfile user={userData} btnTitle="Continue" />
-        </section>
-      </main>
-    );
-  }
-  
-  export default Page;
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccoutProfile user={userData} btnTitle="Continue" />
+      </section>
+    </main>
+  );
+}
+
+export default Page;
--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,31 +1,75 @@
-"use client"
+"use client";
 
+import { Button } from "@/components/ui/button";
 import {
-    Form
-  } from "@/components/ui/form";
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
+import { UserValidation } from "@/lib/validations/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 import { useForm } from "react-hook-form";
 
 interface Props {
-    user: {
-      id: string;
-      objectId: string;
-      username: string;
-      name: string;
-      bio: string;
-      image: string;
-    };
-    btnTitle: string;
-  }
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      Profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
 
-    const form = useForm()
-    return (
-        <Form>
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>This is your public display name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+    );
+};
 
-        </Form>
-    )
-}
+
+
+
 
 export default AccountProfile;
