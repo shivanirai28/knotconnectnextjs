68a29873b31b17ba21950e366265b2c2265e2197
DELL <shivanikumari28@gmail.com>
2024-01-20 02:41:41 +0530

    update

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,1 +1,30 @@
 "use server"
+
+import User from "../models/user.model";
+import {connectToDB} from "../mongoose"
+
+export async function updateUser(
+    userId: string,
+    username:string,
+    name: string,
+    bio: string,
+    image:string,
+    Path: string,
+
+    ): Promise<void> {
+ connectToDB();
+
+ await User.findOneAndUpdate(
+    {id: userId},
+
+    
+    {
+        username:username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded:true,
+    },
+    {upsert:true},
+ )
+}
