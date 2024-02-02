03ce6ece6db881ddfa27ed16b9235dcf1eed7f35
DELL <shivanikumari28@gmail.com>
2024-02-02 23:59:02 +0530

    Profile page ♠️

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,28 @@
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function Page({ params }: { params: { id: string } }) {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  return (
+    <section>
+      <ProfileHeader
+       accountId={userInfo.id}
+       authUserId={user.id}
+       name={userInfo.name}
+       username={userInfo.username}
+       imgUrl={userInfo.image}
+       bio={userInfo.bio}
+     /> 
+    </section>
+  );
+}
+
+export default Page;
