df676ac285279ad2c1dc73d4a8bcf14361b52afe
DELL <shivanikumari28@gmail.com>
2024-02-02 23:25:58 +0530

    reply form updated on page 🎖️

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -34,9 +34,26 @@ const Page = async ({ params }: { params: { id: string } }) => {
       <div className="mt-7">
         <Comment
         threadId={thread.id}
-        currentUserImg={user.imageUrl}
+        currentUserImg={userInfo.image}
         currentUserId={JSON.stringify(userInfo._id)}
         />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user.id}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   );
