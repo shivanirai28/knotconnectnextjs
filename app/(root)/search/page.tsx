f58f680248c805def6ca316ae23e24d1817c3e4c
DELL <shivanikumari28@gmail.com>
2024-02-15 13:41:19 +0530

    complete search section👍

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -4,6 +4,7 @@ import { currentUser } from "@clerk/nextjs";
 
 
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
 
 async function Page({
   searchParams,
@@ -27,7 +28,7 @@ async function Page({
     <section>
       <h1 className='head-text mb-10'>Search</h1>
 
-      <Searchbar routeType='search' />
+     {/* <Searchbar routeType='search' />*/}
 
       <div className='mt-14 flex flex-col gap-9'>
         {result.users.length === 0 ? (
@@ -48,11 +49,11 @@ async function Page({
         )}
       </div>
 
-      <Pagination
+      {/*<Pagination
         path='search'
         pageNumber={searchParams?.page ? +searchParams.page : 1}
         isNext={result.isNext}
-      />
+            />*/}
     </section>
   );
 }
